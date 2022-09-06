b9e32a709f04a844caa87e5023aabfcc4aacc2da
mazwi2 <marcin.zwierzycki@gmail.com>
2022-09-06 21:57:43 +0200

    favicon

---
+++ JS/data.js
@@ -0,0 +1,52 @@
+const work = [
+  {
+    title: `Grafik i przerwa`,
+    description:{
+      1:'Grafik tworzony na cały miesiąc',
+      2:'Życzenia grafikowe na następny miesiąc składamy do 15',
+      3:`Zmiany grafikowe do 24h przed zaczęciem zmiany`,
+      4:`Brak urlopów w miesiące "high" (kwiecień, maj ,wrzesień, grudzień)`,
+      5:`Przerwa 20 min`
+    },
+  },
+  {
+    title: 'Ubiór',
+    description:{
+      1: `Managerowie nie noszą stroju służbowego`,
+      2:`Doradcy klienta noszą koszulki służbowe oraz tag z imieniem`,
+      3:`Pamiętaj, żeby twój strój był zawsze czysty i wyprasowany w myśl zasady "Jak cię widzą, tak cię piszą"`
+    }
+  },
+  {
+    title:'Czego nie robić',
+    description:{
+      1:`Spożywanie posiłków tylko na zapleczu`,
+      2:`O gumie to już wogóle zapomnij`,
+      3:`Telefon zostaw w szafce bo jak kierownik zobaczy go jak używasz na sklepie...(wpisz tu najgorszą rzecz jaką sobie umiesz wyobrazić)`,
+      4:`Bądź profesjonalistą, nie przeklinaj i nie plotkuj`,
+      5:`Używaj radia do pracy`,
+      6:`Jak już palisz, to nie pal w stroju służbowym`
+    }
+  },
+
+]
+
+const BHP = [
+  {
+    title: `Bezpieczeństwo`,
+    description: {
+      1: 'Każdy pracownik przy wyjściu ze sklepu musi pokazać torbę reszcie zespołu (pod kamerą)',
+      2:` `,
+      3:`Zatrzymaj informacje dla siebie, Jako pracownik masz dostęp do danych poufnych, a to oznacza, że musisz być wyjątkowo ostrożny co piszesz w social mediach o firmie. Zakaz wstawiania zdjęć ze sklepu. `,
+    },
+    
+  },
+  {
+    title: `Nieobecności`,
+    description: {
+      1: 'W razie spóźnienia lub nieobecności musisz poinformować sklep przed rozpoczęciem zmiany',
+      2:'Jeżeli nie możesz przyjść do pracy, ponieważ jesteś chory musisz poinformować sklep oraz otrzymać zwolnienie od lekarza. Brak zwolnienia skutkuje nieobecnością nieusprawiedliwioną niepłatną ',
+      
+    }
+  },
+];
