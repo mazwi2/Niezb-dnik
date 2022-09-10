c347d7c0d852bda1fca64822211b91b43106873c
mazwi2 <marcin.zwierzycki@gmail.com>
2022-09-10 18:52:49 +0200

    changes in the functionality

--- JS/app.js
+++ JS/app.js
@@ -1,3 +1,6 @@
+// *********** open sidebar ************
+
+// select items
 const sidebar = document.querySelector('.sidebar');
 const openBtn = document.querySelector('.open-btn');
 const leftIcon = document.querySelector('#left');
@@ -6,7 +9,7 @@ const rightIcon = document.querySelector('#right');
 // open sidebar addEventListener
 openBtn.addEventListener('click', openSidebar);
 
-// functions
+// function
 
 function openSidebar() {
   if (sidebar.classList.contains('sidebar-close')) {
@@ -23,3 +26,65 @@ function openSidebar() {
     rightIcon.classList.remove('invisible');
   }
 }
+
+// *********** end of open sidebar ************
+
+// *********** show info ************
+
+// select items
+
+const sidebarLinks = document.querySelectorAll('.link')
+const mainCenter = document.querySelector('.main-center')
+
+// show items
+
+function showTitle(data){
+  let dispalyTitle = data.map((item)=>{
+    return `
+    <h3 class="title" data-number="${item.number}" data-id="${item.id}">${item.title}</h3>
+    `;
+  })
+  dispalyTitle = dispalyTitle.join("");
+  mainCenter.innerHTML = dispalyTitle;
+
+  // show specific info
+  const headingLinks = document.querySelectorAll('.title');
+  headingLinks.forEach((link) => {
+    link.addEventListener('click',(e) => {
+      const category = e.currentTarget.dataset.number;
+      const workDescription = work.filter((workItem) => {
+        // const {id,description} = work
+        if (workItem.number === category) {
+          console.log(workItem.description);
+          return workItem.description;
+        }
+      });
+      let dispalyInfo = workDescription.map((item) => {
+          return `
+          <ul>
+            ${item.description}
+          </ul>
+          `;
+      });
+      dispalyInfo = dispalyInfo.join('');
+      mainCenter.innerHTML = dispalyInfo;
+    })
+  })
+}
+
+// filter items
+
+sidebarLinks.forEach((link) => {
+  link.addEventListener('click', openSidebar)
+  link.addEventListener('click', (e)=>{
+    const category = e.currentTarget.dataset.id;
+    const workTitle = work.filter((workItem)=>{
+      if(workItem.id === category){
+        return workItem.title
+      }
+    })
+    showTitle(workTitle)
+    
+  })
+})
+
--- JS/data.js
+++ JS/data.js
@@ -1,52 +1,33 @@
 const work = [
   {
+    id: `job`,
+    number: `1`,
     title: `Grafik i przerwa`,
-    description:{
-      1:'Grafik tworzony na cały miesiąc',
-      2:'Życzenia grafikowe na następny miesiąc składamy do 15',
-      3:`Zmiany grafikowe do 24h przed zaczęciem zmiany`,
-      4:`Brak urlopów w miesiące "high" (kwiecień, maj ,wrzesień, grudzień)`,
-      5:`Przerwa 20 min`
-    },
+    description: `<li>Grafik tworzony na cały miesiąc</li> <li>Życzenia grafikowe na następny miesiąc składamy do 15</li> <li>Zmiany grafikowe do 24h przed zaczęciem zmiany</li> <li>Brak urlopów w miesiące "high" (kwiecień, maj ,wrzesień, grudzień)</li> <li>Przerwa 20 min</li>`,
   },
   {
+    id: `job`,
+    number: `2`,
     title: 'Ubiór',
-    description:{
-      1: `Managerowie nie noszą stroju służbowego`,
-      2:`Doradcy klienta noszą koszulki służbowe oraz tag z imieniem`,
-      3:`Pamiętaj, żeby twój strój był zawsze czysty i wyprasowany w myśl zasady "Jak cię widzą, tak cię piszą"`
-    }
+    description: `<li>Managerowie nie noszą stroju służbowego</li> <li>Doradcy klienta noszą koszulki służbowe oraz tag z imieniem</li> <li>Pamiętaj, żeby twój strój był zawsze czysty i wyprasowany w myśl zasady "Jak cię widzą, tak cię piszą"</li>`,
   },
   {
-    title:'Czego nie robić',
-    description:{
-      1:`Spożywanie posiłków tylko na zapleczu`,
-      2:`O gumie to już wogóle zapomnij`,
-      3:`Telefon zostaw w szafce bo jak kierownik zobaczy go jak używasz na sklepie...(wpisz tu najgorszą rzecz jaką sobie umiesz wyobrazić)`,
-      4:`Bądź profesjonalistą, nie przeklinaj i nie plotkuj`,
-      5:`Używaj radia do pracy`,
-      6:`Jak już palisz, to nie pal w stroju służbowym`
-    }
+    id: `job`,
+    number: `3`,
+    title: 'Czego nie robić',
+    description: `<li>Spożywanie posiłków tylko na zapleczu O gumie to już wogóle zapomnij</li> <li>Telefon zostaw w szafce bo jak kierownik zobaczy go jak używasz na sklepie...(wpisz tu najgorszą rzecz jaką sobie umiesz wyobrazić)</li><li> Bądź profesjonalistą, nie przeklinaj i nie plotkuj </li> <li> Używaj radia do pracy Jak już palisz, to nie pal w stroju służbowym </li>`,
   },
-
-]
-
-const BHP = [
   {
+    id: `BHP`,
+    number: `4`,
     title: `Bezpieczeństwo`,
-    description: {
-      1: 'Każdy pracownik przy wyjściu ze sklepu musi pokazać torbę reszcie zespołu (pod kamerą)',
-      2:` `,
-      3:`Zatrzymaj informacje dla siebie, Jako pracownik masz dostęp do danych poufnych, a to oznacza, że musisz być wyjątkowo ostrożny co piszesz w social mediach o firmie. Zakaz wstawiania zdjęć ze sklepu. `,
-    },
-    
+    description: `<li>Każdy pracownik przy wyjściu ze sklepu musi pokazać torbę reszcie zespołu (pod kamerą)</li> <li>Zatrzymaj informacje dla siebie, Jako pracownik masz dostęp do danych poufnych, a to oznacza, że musisz być wyjątkowo ostrożny co piszesz w social mediach o firmie. Zakaz wstawiania zdjęć ze sklepu.</li> `,
   },
   {
+    id: `BHP`,
+    number: `5`,
     title: `Nieobecności`,
-    description: {
-      1: 'W razie spóźnienia lub nieobecności musisz poinformować sklep przed rozpoczęciem zmiany',
-      2:'Jeżeli nie możesz przyjść do pracy, ponieważ jesteś chory musisz poinformować sklep oraz otrzymać zwolnienie od lekarza. Brak zwolnienia skutkuje nieobecnością nieusprawiedliwioną niepłatną ',
-      
-    }
+    description: `<li>W razie spóźnienia lub nieobecności musisz poinformować sklep przed rozpoczęciem zmiany</li> <li>Jeżeli nie możesz przyjść do pracy, ponieważ jesteś chory musisz poinformować sklep oraz otrzymać zwolnienie od lekarza. Brak zwolnienia skutkuje nieobecnością nieusprawiedliwioną niepłatną </li>`,
   },
 ];
+
